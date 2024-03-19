314db8318d2166c4ba80052769bcce47fd77cd35
pknguyen2411 <163966578+pknguyen2411@users.noreply.github.com>
2024-03-19 14:44:38 +0900

    Add files via upload

--- numpytest.py
+++ numpytest.py
@@ -4,7 +4,29 @@ print("my numpy version: ", np.__version__)
 
 
 randomList = []
-for i in range(100000):
+for i in range(100_000):
     n = np.random.randint(1,20)
     randomList.append(n)
-print("randomList: ", randomList)
+# print("randomList: ", randomList)
+
+randomArray = np.random.randint(1, 20, size=10_000_000)
+print("randomArray: ", randomArray.shape, randomArray.size)
+
+
+# let's find out if there is 19 in the list.
+count = 0
+for number in randomArray:
+    if number == 19:
+        count += 1
+        # print("co 19 nha thay")
+# print("the list contains", count, "19s.")
+# print(f"the list contains {count} 19s.")
+
+for n in range(1,20):
+    countAll = 0
+    for number in randomArray:
+        if number == n:
+            countAll += 1
+    print(n, ":", f"{countAll}")
+
+print("Finished.")
