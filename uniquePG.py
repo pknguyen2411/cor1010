06071d75e7c040a18c048f25358a2250ccdc3b6e
pknguyen2411 <163966578+pknguyen2411@users.noreply.github.com>
2024-03-26 14:44:59 +0900

    Add files via upload

---
+++ uniquePG.py
@@ -0,0 +1,18 @@
+filename = "pride2.txt"
+
+filehandler = open(filename, "r", encoding='utf-8') # w = write, r = read
+
+n = 0
+uniqueLetters = []
+while True:
+    line = filehandler.readline()
+    if not line:
+        break
+    n += 1
+    for letters in line:
+        if letters not in uniqueLetters:
+            uniqueLetters.append(letters)
+
+print("unique letters =", len(uniqueLetters), uniqueLetters)
+
+print(n)
